79f9f313ea4d4a5f0f21aa013e3dbff02ce92e60
SFKia359 <ethanmajor117@gmail.com>
2024-11-22 23:04:48 -0700

    Added npm install fetch-node and OpenweatherMap, fetch data, and fetch data for weather data to get the tempand city

---
+++ script.js
@@ -0,0 +1,36 @@
+// OpenWeatherMap API Key
+const apiKey = "b13852367f55131884d9289580dedd31";
+
+// Change to your city of choice
+const city = "Calgary";
+
+// URL with string interpolation to insert city and API key
+const apiurl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+
+// fetch data
+const fetchData = async (url) => {
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error("Failed to fetch data");
+    }
+
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Error fetching data:", error);
+  }
+};
+
+fetchData(apiurl).then((data) => {
+  if (data) {
+    console.log("Weather Data:", data);
+    const weather = data.weather[0].description;
+    const temperature = data.main.temp - 273.15;
+    console.log(
+      `The weather in ${city} is ${weather} with a temperature of ${temperature.toFixed(
+        2
+      )}°C.`
+    );
+  }
+});
